bf43432f0cfe1b0faac22c5351db645ad5ff2299
tsc <mz260959@gmail.com>
2024-08-29 11:01:58 +0500

    power

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,20 +7,14 @@ const page = () => {
     <div className="pt-6 md:p-8 text-center space-y-4">
       <blockquote>
         <p className="text-lg font-medium">
-        That's great! Gardening is a wonderful hobby that can bring joy, relaxation, and a sense of accomplishment. It's also a great way to connect with nature and grow your own food. What specific aspects of gardening interest you the most? Are you looking to:
-
-- Learn about different types of plants and flowers?
-- Get tips on gardening techniques and best practices?
-- Explore garden design and layout ideas?
-- Understand how to care for specific plants or troubleshoot common issues?
-- Learn about hydroponics, aeroponics, or other forms of indoor gardening?
-
-Let me know, and I'll be happy to help you with your gardening interests!.
+        Tailwind CSS is the only framework that I have seen scale
+          on large teams. It is easy to customize, adapts to any design,
+          and the build size is tiny.
         </p>
       </blockquote>
       <figcaption className="font-medium">
         <div className="text-sky-500 dark:text-sky-400">
-          Miss Waresha
+          Sarah Dayan
         </div>
         <div className="text-slate-700 dark:text-slate-500">
           Staff Engineer, Algolia
